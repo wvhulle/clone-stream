6caeb54555bef26b54be67dd2220beb7876135d8
Willem Vanhulle <wvhulle@users.noreply.github.com>
2025-04-08 14:16:52 +0200

    send two

--- tests/two_forks.rs
+++ tests/two_forks.rs
@@ -56,3 +56,65 @@
          assert_eq!(fork2.next_a(), Poll::Ready(Some(())));
     }
     
+    
+      #[test]
+    fn both_pending_send_two_receive_one() {
+        let ForkAsyncMockSetup {
+            mut sender, forks, ..
+        } = ForkAsyncMockSetup::<(), 2>::new();
+
+        let [mut fork1, mut fork2] = forks;
+
+        assert_eq!(fork1.next_a(), Poll::Pending);
+        assert_eq!(fork2.next_a(), Poll::Pending);
+        block_on(async {
+            let _ = sender.send(()).await;
+        });
+        assert_eq!(fork1.next_a(), Poll::Ready(Some(())));
+        block_on(async {
+            let _ = sender.send(()).await;
+        });
+         assert_eq!(fork1.next_a(), Poll::Ready(Some(())));
+    }
+       #[test]
+    fn both_pending_send_two_receive_one_late() {
+        let ForkAsyncMockSetup {
+            mut sender, forks, ..
+        } = ForkAsyncMockSetup::<usize, 2>::new();
+
+        let [mut fork1, mut fork2] = forks;
+
+        assert_eq!(fork1.next_a(), Poll::Pending);
+        assert_eq!(fork2.next_a(), Poll::Pending);
+        block_on(async {
+            let _ = sender.send(0).await;
+        });
+        assert_eq!(fork1.next_a(), Poll::Ready(Some(0)));
+        block_on(async {
+            let _ = sender.send(1).await;
+        });
+         assert_eq!(fork2.next_a(), Poll::Ready(Some(0)));
+    }
+    
+    
+          #[test]
+    fn both_pending_send_two_receive_two_twice() {
+        let ForkAsyncMockSetup {
+            mut sender, forks, ..
+        } = ForkAsyncMockSetup::<usize, 2>::new();
+
+        let [mut fork1, mut fork2] = forks;
+
+        assert_eq!(fork1.next_a(), Poll::Pending);
+        assert_eq!(fork2.next_a(), Poll::Pending);
+        block_on(async {
+            let _ = sender.start_send(0).await;
+            let _ = sender.start_send(0).await;
+            sender.flush().await;
+        });
+    
+         assert_eq!(fork1.next_a(), Poll::Ready(Some(0)));
+          assert_eq!(fork2.next_a(), Poll::Ready(Some(0)));
+           assert_eq!(fork1.next_a(), Poll::Ready(Some(1)));
+         assert_eq!(fork2.next_a(), Poll::Ready(Some(1)));
+    }
