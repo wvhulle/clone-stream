519a04cf3537a9cfeae3fd711a6357b8ee93785b
Willem Vanhulle <wvhulle@users.noreply.github.com>
2025-04-08 14:04:29 +0200

    remove duplicate

--- tests/two_forks.rs
+++ tests/two_forks.rs
@@ -38,24 +38,6 @@
         assert_eq!(fork2.next_a(), Poll::Pending);
     }
 
-   #[test]
-    fn one_pending_send_one() {
-        let ForkAsyncMockSetup {
-            mut sender, forks, ..
-        } = ForkAsyncMockSetup::<(), 2>::new();
-
-        let [mut fork1, mut fork2] = forks;
-
-        assert_eq!(fork1.next_a(), Poll::Pending);
-       
-
-        block_on(async {
-            let _ = sender.send(()).await;
-        });
-        assert_eq!(fork1.next_a(), Poll::Ready(Some(())));
-
-        assert_eq!(fork2.next_a(), Poll::Pending);
-    }
     #[test]
     fn both_pending_send_one() {
         let ForkAsyncMockSetup {
