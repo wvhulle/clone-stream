485e45a4888e005a10128e14c1f60c394be9dbdf
Willem Vanhulle <willemvanhulle@gmail.com>
2025-04-18 14:38:18 +0200

    Simplify tests

--- tests/futures_mpsc.rs
+++ tests/futures_mpsc.rs
@@ -1,50 +1,49 @@
 use clone_stream::CloneStream;
 use futures::{
-    SinkExt, StreamExt,
+    StreamExt,
     executor::{ThreadPool, block_on},
     future::join_all,
-    join,
+    join, stream,
     task::SpawnExt,
 };
+
+const N_STREAM_CLONES: usize = 500;
+
+const N_ITEMS_SENT: usize = 5000;
+
 #[test]
 fn mass_send() {
-    let (mut sender, rx) = futures::channel::mpsc::unbounded::<usize>();
+    let (sender, receiver) = futures::channel::mpsc::unbounded::<usize>();
 
-    let template_clone: CloneStream<_> = rx.into();
+    let template_clone: CloneStream<_> = receiver.into();
     let pool = ThreadPool::builder().create().unwrap();
-    let n_clones = 500;
 
-    let n_items = 5000;
+    let expect_numbers = (0..N_ITEMS_SENT).collect::<Vec<_>>();
 
-    let expected_received = (0..n_items).collect::<Vec<_>>();
-
-    let mut join_handles = Vec::new();
-
-    for i in 0..n_clones {
-        let receive_fut = template_clone.clone().collect::<Vec<_>>();
-        let expected = expected_received.clone();
-        let handle = pool
-            .spawn_with_handle(async move {
-                assert_eq!(
-                    receive_fut.await,
-                    expected,
-                    "Fork {} received unexpected items",
-                    i + 1
-                );
-            })
-            .unwrap();
-        join_handles.push(handle);
-    }
+    let wait_for_receive_all = join_all((0..N_STREAM_CLONES).map(|i| {
+        let receive_all = template_clone.clone().collect::<Vec<_>>();
+        let expected = expect_numbers.clone();
+        pool.spawn_with_handle(async move {
+            assert_eq!(
+                receive_all.await,
+                expected,
+                "Fork {} received unexpected items",
+                i + 1
+            );
+        })
+        .unwrap()
+    }));
 
     let send = pool
         .spawn_with_handle(async move {
-            for i in 0..n_items {
-                sender.send(i).await.unwrap();
-            }
+            (stream::iter(0..N_ITEMS_SENT).map(Ok))
+                .forward(sender)
+                .await
+                .unwrap();
         })
         .unwrap();
 
     block_on(async move {
-        join!(send, join_all(join_handles));
+        join!(send, wait_for_receive_all);
     });
 }
