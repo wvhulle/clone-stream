6a870bc25dd4d7f284e314052089e5366b6fcf70
Willem Vanhulle <wvhulle@users.noreply.github.com>
2025-09-22 13:51:46 +0200

    Remove unused error variants (#28)

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
 /// Errors that can occur when working with cloned streams
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum CloneStreamError {
     /// The maximum number of clones has been exceeded
@@ -8,15 +9,6 @@ pub enum CloneStreamError {
         max_allowed: usize,
         current_count: usize,
     },
-    /// The maximum queue size has been exceeded
-    MaxQueueSizeExceeded {
-        max_allowed: usize,
-        current_size: usize,
-    },
-    /// Attempted to access a clone that doesn't exist
-    CloneNotFound { clone_id: usize },
-    /// The fork is in an invalid state
-    InvalidForkState { message: String },
 }
 
 impl fmt::Display for CloneStreamError {
@@ -29,24 +21,10 @@ impl fmt::Display for CloneStreamError {
                 f,
                 "Maximum number of clones exceeded: {current_count} >= {max_allowed}"
             ),
-            CloneStreamError::MaxQueueSizeExceeded {
-                max_allowed,
-                current_size,
-            } => write!(
-                f,
-                "Maximum queue size exceeded: {current_size} >= {max_allowed}"
-            ),
-            CloneStreamError::CloneNotFound { clone_id } => {
-                write!(f, "Clone with ID {clone_id} not found")
-            }
-            CloneStreamError::InvalidForkState { message } => {
-                write!(f, "Invalid fork state: {message}")
-            }
         }
     }
 }
 
 impl std::error::Error for CloneStreamError {}
 
-/// Result type for clone stream operations
 pub type Result<T> = std::result::Result<T, CloneStreamError>;
